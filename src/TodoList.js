57afcff32ca6e61cd4500706a2ca6aba07990869
Astha Verma <astha24verma@gmail.com>
2024-02-23 15:09:47 +0530

    Updated feature of local storage

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function TodoList() {
-    const [task, setTask] = useState("")
-    const [listData, setListData] = useState([])
+    const [task, setTask] = useState('');
+
+    // Fetch from local storage
+    const [listData, setListData] = useState(() => {
+        return JSON.parse(localStorage.getItem('listData')) || []
+    })
+
+    // Fetch from local storage - not working
+    // useEffect(() => {
+    //     const listData = JSON.parse(localStorage.getItem('listData')) || []
+    //     setListData(listData)
+    // }, [])
 
     function addTask() {
-        if(task.length === 0) return;
+        if (task.length === 0) return;  // if task is empty, return 
         setListData((listData) => {
             const updatedListData = [...listData, task]
             setTask("")
-            console.log(updatedListData)
-            return updatedListData
+            console.log("updated : " + updatedListData)
+            return updatedListData  // return updated listData
         })
     }
+
+    console.log(listData);
 
     function removeTask(i) {
         const updatedListDataAfterRemoving = listData.filter((elem, id) => {
@@ -24,13 +36,21 @@ function TodoList() {
     function removeAllTask() {
         setListData([])
     }
+
+    // Add to local storage
+    useEffect(() => {
+        localStorage.setItem('listData', JSON.stringify(listData))
+    }, [listData])
+
+    console.log("Added to local storage ")
+
     return (
         <>
             <header className="head">TO DO List</header>
             <input type='text' placeholder='Add your task' value={task} onChange={event => setTask(event.target.value)} />
             <button onClick={addTask}>Add</button>
             <div className='space'></div>
-            
+
             {listData != [] && listData.map((data, i) => {
                 return (
                     <div>
